057d5e1c7f3affe1e5ef36de0bd1b57e2ca5e110
Rain <gxl405@case.edu>
2023-04-16 17:27:22 -0400

    完成整体工作，剩下的就是易用性的优化问题了

--- ServerStarter.py
+++ ServerStarter.py
@@ -1,23 +1,13 @@
 import Server
+from configparser import ConfigParser
 
 if __name__ == '__main__':
+    config = ConfigParser()
+    config.read('config.ini')
+    idList = config.sections()
     myserver = Server.Server('127.0.0.1', 5555)
-    disvec = {'x': 5, 'w': 3, 'v': 7, 'y': -1, 'z': -1}
-    myserver.init_add_one_to_DV_tabel('u', disvec)
-
-    disvec = {'u': 5, 'w': 4, 'v': -1, 'y': 7, 'z': 9}
-    myserver.init_add_one_to_DV_tabel('x', disvec)
-
-    disvec = {'u': 3, 'x': 4, 'v': 3, 'y': 8, 'z': -1}
-    myserver.init_add_one_to_DV_tabel('w', disvec)
-
-    disvec = {'u': 7, 'x': -1, 'w': 3, 'y': 4, 'z': -1}
-    myserver.init_add_one_to_DV_tabel('v', disvec)
-
-    disvec = {'u': -1, 'x': 7, 'w': 8, 'v': 4, 'z': 2}
-    myserver.init_add_one_to_DV_tabel('y', disvec)
-
-    disvec = {'u': -1, 'x': 9, 'w': -1, 'v': -1, 'y': 2}
-    myserver.init_add_one_to_DV_tabel('z', disvec)
+    for i in idList:
+        c = eval(config[i]['pairs'])
+        myserver.init_add_one_to_DV_tabel(i, c)
 
     myserver.serverStart()
